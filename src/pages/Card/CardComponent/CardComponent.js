05d915fcf36e7c69896eab470b6074b608b9831f
tomascdmota <tomascdmota@gmail.com>
2023-12-10 12:20:01 +0000

    vcard working on mobile again, images are now with greater quality

--- src/pages/Card/CardComponent/CardComponent.js
+++ src/pages/Card/CardComponent/CardComponent.js
@@ -14,6 +14,7 @@ import GoogleReviewsLogo from './Logos/googlereview.png';
 import UrlLogo from './Logos/url.png';
 import YouTubeLogo from "./Logos/youtube.svg";
 
+
 import './CardComponent.css';
 
 function CardComponent({
@@ -28,8 +29,8 @@ function CardComponent({
   facebook,
   linkedin,
   instagram,
-  youtube,
   google_reviews,
+  youtube,
   notes,
   url,
 }) {
@@ -37,9 +38,9 @@ function CardComponent({
   const [loading, setLoading] = useState(true);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const accessKeyId = process.env.REACT_APP_AWS_ACCESS_KEY_ID;
-  const secretAccessKey = process.env.REACT_APP_AWS_SECRET_ACCESS_KEY;
-  const Region = process.env.REACT_APP_S3_REGION;
-  const Bucket = process.env.REACT_APP_BUCKET;
+  const secretAccessKey =  process.env.REACT_APP_AWS_SECRET_ACCESS_KEY;
+  const Region =  process.env.REACT_APP_S3_REGION;
+  const Bucket =  process.env.REACT_APP_BUCKET;
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -66,28 +67,30 @@ function CardComponent({
     }
   };
 
+ 
+
   const saveImageToIndexedDB = async (imageBlob) => {
     try {
       const db = await initIndexedDB();
       const tx = db.transaction('images', 'readwrite');
       const store = tx.objectStore('images');
-
+  
       // Use a unique key, such as the S3 key, assuming it's unique for each image
       const key = 'profileImage';
-
+      
       // Create an object with a URL (if needed) and the image data
       const data = {
         key,
-        url: profile_image_url, // Add the URL if it's necessary for your use case
+        url: profile_image_url,  // Add the URL if it's necessary for your use case
         data: imageBlob,
       };
-
+  
       // Store the data in IndexedDB
       store.put(data);
-
+  
       // Complete the transaction
       await tx.complete;
-
+  
       console.log('Image stored in IndexedDB:', data);
     } catch (error) {
       console.error('Error storing image in IndexedDB:', error);
@@ -100,7 +103,7 @@ function CardComponent({
       const tx = db.transaction('images');
       const store = tx.objectStore('images');
       const entry = await store.get('profileImage');
-
+  
       return entry ? entry.data : null;
     } catch (error) {
       console.error('Error loading from IndexedDB:', error);
@@ -110,15 +113,6 @@ function CardComponent({
 
   const fetchImage = async () => {
     try {
-      // If there is no profile_image_url, skip fetching image
-      if (!profile_image_url) {
-        setLoading(false);
-        if (onLoad) {
-          onLoad();
-        }
-        return;
-      }
-
       const s3Client = new S3({
         credentials: {
           accessKeyId,
@@ -126,25 +120,27 @@ function CardComponent({
         },
         region: Region,
       });
-
+  
       // Parse the profile_image_url to extract the S3 key
       const urlObject = new URL(profile_image_url);
       const key = decodeURIComponent(urlObject.pathname.replace(/^\//, ''));
-
+  
       const getObjectParams = {
         Bucket,
         Key: key,
       };
-
+  
       const command = new GetObjectCommand(getObjectParams);
       const response = await s3Client.send(command);
-
+  
+     
+  
       const blob = new Blob([response.Body], { type: response.ContentType });
       saveImageToIndexedDB(blob);
       // Save the image Blob directly to the state
       setImage(blob);
       setLoading(false);
-
+  
       if (onLoad) {
         onLoad();
       }
@@ -179,41 +175,38 @@ function CardComponent({
     };
   }, [profile_image_url, Bucket, Region, accessKeyId, secretAccessKey, onLoad]);
 
+
+
   const saveToContacts = async () => {
     try {
       const card = new vcf();
       const imageBuffer = await loadImageFromIndexedDB();
-
+  
       if (!imageBuffer) {
         console.error('Image not found in IndexedDB.');
         return;
       }
-
+  
       // Set basic properties
-
+  
       // Manually encode special characters in the names
       const removeDiacritics = (str) => unorm.nfkd(str).replace(/[\u0300-\u036f]/g, '');
       const decodedUsername = removeDiacritics(decodeURIComponent(username));
-      const decodedTitle = removeDiacritics(decodeURI(title))
+      const decodedTitle = removeDiacritics(decodeURIComponent(title));
       // Set structured name with properly encoded values
       card.add('n', [decodedUsername]);
-
+  
       // Set formatted name with the full, properly encoded name
       card.add('fn', [decodedUsername]);
-
+  
       card.add('org', company);
       card.add('tel', phone);
       card.add('email', email);
-
+  
       // Set additional properties
-    
-
-      // ...
-
-    
-      card.add('title', decodedTitle);
+      card.add('title', [decodedTitle]);
       card.add('url', url);
-
+  
       // Set social media properties
       if (facebook) {
         card.add('x-socialprofile', facebook, { type: 'Facebook' });
@@ -224,70 +217,67 @@ function CardComponent({
       if (linkedin) {
         card.add('x-socialprofile', linkedin, { type: 'Linkedin' });
       }
-      if (youtube) {
-        card.add('x-socialprofile', youtube, { type: 'Youtube' });
-      }
-      if (google_reviews) {
-        card.add('x-socialprofile', google_reviews, { type: 'Google' });
-      }
-      if(notes) {
-        card.add('')
-      }
-
+  
       // Extract the S3 key from the profile_image_url
       const urlObject = new URL(profile_image_url);
       const imageKey = decodeURIComponent(urlObject.pathname.replace(/^\//, ''));
-
+  
       // Generate a pre-signed URL for the image
       const imageURL = await generatePresignedURL(imageKey);
-
-      // Set image URL in vCard directly
-      card.add('photo', imageURL);
-
+  
+      const encodedImage = await fetchAndEncodeImage(imageURL);
+      if (!encodedImage) {
+        console.error('Error fetching and encoding image.');
+        return;
+      }
+  
+      // Set image in vCard
+      card.add('photo', encodedImage, { encoding: 'b', type: 'image/jpeg' });
+  
       // Generate vCard data as a string
       const vCardData = card.toString();
       console.log(vCardData);
-
+  
       // Create a Blob from the vCard data with explicit UTF-8 encoding
       const vcard_blob = new Blob([new TextEncoder().encode(vCardData)], {
         type: 'text/vcard;charset=utf-8',
       });
-
+  
       // Open a new URL to prompt the user to add the contact
       const downloadLink = document.createElement('a');
       downloadLink.href = URL.createObjectURL(vcard_blob);
       downloadLink.download = 'contact.vcf';
-
+  
       // Trigger a click event to simulate a download prompt
       downloadLink.click();
-
+  
       // Release the object URLs
       URL.revokeObjectURL(downloadLink.href);
     } catch (error) {
       console.error('Error saving to contacts:', error);
     }
   };
-
+  
   // Function to fetch and encode the image
   const fetchAndEncodeImage = async (imageUrl) => {
     try {
       const response = await fetch(imageUrl);
       const imageBlob = await response.blob();
-
+  
       // Resize and compress the image with a lower quality
       const resizedAndCompressedBase64 = await resizeAndCompressImage(imageBlob, {
-        maxWidth: 200,
-        maxHeight: 200,
-        quality: 0.8, // Adjust the quality as needed
+        maxWidth: 800,
+        maxHeight: 800,
+        quality: 0.9, // Adjust the quality as needed
       });
-
+  
       return resizedAndCompressedBase64;
     } catch (error) {
       console.error('Error fetching, resizing, and encoding image:', error);
       return null;
     }
   };
-
+  
   const resizeAndCompressImage = async (blob, options) => {
     try {
       const image = new Image();
@@ -298,61 +288,53 @@ function CardComponent({
         image.onerror = reject;
       });
   
-      const { maxWidth, maxHeight, quality, maxFileSize } = options;
+      const { maxWidth, maxHeight, quality } = options;
   
-      // Calculate new dimensions while maintaining the aspect ratio
       let width = image.width;
       let height = image.height;
   
-      if (width > maxWidth) {
-        height *= maxWidth / width;
-        width = maxWidth;
+      if (width > height) {
+        if (width > maxWidth) {
+          height *= maxWidth / width;
+          width = maxWidth;
+        }
+      } else {
+        if (height > maxHeight) {
+          width *= maxHeight / height;
+          height = maxHeight;
+        }
       }
   
-      if (height > maxHeight) {
-        width *= maxHeight / height;
-        height = maxHeight;
-      }
-  
-      // Create a new canvas with the final dimensions
       const canvas = document.createElement('canvas');
       canvas.width = width;
       canvas.height = height;
       const context = canvas.getContext('2d');
   
-      // Draw the resized image on the canvas
+      // Draw the image on the canvas progressively
       context.drawImage(image, 0, 0, width, height);
   
-      // Convert the canvas content to a Blob with additional compression
+      // Convert the canvas content to a blob
       const resizedBlob = await new Promise((resolve) => {
         canvas.toBlob(resolve, 'image/jpeg', quality);
       });
   
-      // Check if the file size is within the specified limit
-      if (resizedBlob.size > maxFileSize) {
-        return resizeAndCompressImage(resizedBlob, {
-          maxWidth,
-          maxHeight,
-          quality: quality - 0.1, // Adjust compression quality
-          maxFileSize,
-        });
-      }
+      // Convert the resized blob to base64
+      const resizedBlobBuffer = await new Response(resizedBlob).arrayBuffer();
+      const resizedBlobBase64 = btoa(
+        new Uint8Array(resizedBlobBuffer).reduce((data, byte) => data + String.fromCharCode(byte), '')
+      );
   
-      return resizedBlob;
+      return resizedBlobBase64;
     } catch (error) {
       console.error('Error resizing and compressing image:', error);
       return null;
     }
   };
-
+  
+  
+  
   const generatePresignedURL = async (imageKey) => {
     try {
-      // If there is no profile_image_url, return a default placeholder URL
-      if (!profile_image_url) {
-        // Replace 'https://placekitten.com/200/200' with your desired placeholder URL
-        return 'https://placekitten.com/200/200';
-      }
-
       const s3Client = new S3({
         region: 'eu-west-2',
         credentials: {
@@ -360,35 +342,23 @@ function CardComponent({
           secretAccessKey: secretAccessKey,
         },
       });
-
+  
       const params = {
         Bucket: 'brava-bucket',
         Key: imageKey,
         Expires: 900,
       };
-
-      // Generate a pre-signed URL
-      const signedURL = await getSignedUrl(s3Client, new GetObjectCommand(params));
   
-      // Fetch the image to get the base64-encoded representation
-      const response = await fetch(signedURL);
-      const imageBlob = await response.blob();
-  
-      // Convert the Blob to base64 using FileReader
-      const base64EncodedImage = await new Promise((resolve) => {
-        const reader = new FileReader();
-        reader.onloadend = () => {
-          resolve(reader.result.split(',')[1]);
-        };
-        reader.readAsDataURL(imageBlob);
-      });
-  
-      return base64EncodedImage;
+      const signedURL =  getSignedUrl(s3Client, new GetObjectCommand(params));
+      console.log(signedURL)
+      return signedURL;
     } catch (error) {
       console.error('Error generating pre-signed URL:', error);
       throw error;
     }
   };
+
+
 
   const handleGetInTouch = () => {
     try {
@@ -398,7 +368,6 @@ function CardComponent({
       console.error('Error opening modal:', error);
     }
   };
-
   return (
     <div className={`card-component ${loading ? 'loading' : ''}`}>
       <div className="card-background" style={{ backgroundImage: `url(${background_image_url})` }}></div>
