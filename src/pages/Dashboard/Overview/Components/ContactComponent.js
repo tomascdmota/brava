49462215874bee96a0053153c2ae9cdee0453cdd
tomascdmota <tomascdmota@gmail.com>
2024-04-26 19:25:54 +0100

    Created leads Component, added lead filtering and sorting. \n Updated Side Navigation

--- src/pages/Dashboard/Overview/Components/ContactComponent.js
+++ src/pages/Dashboard/Overview/Components/ContactComponent.js
@@ -1,50 +1,66 @@
-import React from 'react'
-import './ContactComponent.scss'
+import React, { useState } from 'react';
+import './ContactComponent.scss';
 
+function ContactComponent({ user_id, contact_id, name, company, email, message, contact_date, phone, sector }) {
+  const isMobile = window.innerWidth <= 1000;
+  const formattedDate = new Date(contact_date).toLocaleDateString();
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
- function ContactComponent({user_id, contact_id, name, company, email, message,contact_date}) {
-    const isMobile = window.innerWidth <= 1000;
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
 
-   
   return (
-    <div>
-        
     <div className="contacts">
-        <div className="contact">
-        { /* !isMobile && <img src="https://cdn.shopify.com/s/files/1/0733/7767/7577/files/profile_2.png?v=1712073839"/>  */}
-        {isMobile && (
-              <div>
-                <dt>Contact Date</dt>
-                <dd>{contact_date}</dd>
-              </div>
-            )}
-            <dl className="contact-details">
-                <div>
-                    <dt>{name}, {company}</dt>
-                </div>
-                
-                <div>
-                    
-                    <dd>{email}</dd>
-                </div>
-                {!isMobile && (
-              <div>
-                <dd>{contact_date}</dd>
-              </div>
-            )}
-                 <div className={`contact-message ${isMobile ? 'full-width-message' : ''}`}>
-                  <dt>{message}</dt>
-                </div>
-            </dl>
-           {/*<div className="reply">
-                <button>Responder</button> 
-                </div>*/}
+      <div className="contact">
+        <dl className="contact-details">
+          <div>
+            <dt>{name}, {company}</dt>
+          </div>
+          <div>
+            <dd style={{marginRight:"80px"}}>{email}</dd>
+          </div>
+          <div>
+            <dd>{phone}</dd>
+          </div>
+          <div>
+            <dd>{sector}</dd>
+          </div>
+          {!isMobile && (
+            <div>
+              <dd style={{marginLeft:'-10px'}}>{formattedDate}</dd>
+            </div>
+          )}
+          <div className={`contact-message ${isMobile ? 'full-width-message' : ''}`}>
+            <dt>
+                <button onClick={toggleModal}>Read Message</button>
+            </dt>
+          </div>
+        </dl>
+      </div>
+      {isModalOpen && (
+      <div className="lead-modal">
+      <div className="lead-modal-content">
+        <span className="close" onClick={toggleModal}>&times;</span>
+        <h2 className='lead-modal-name'>Message</h2>
+        <p>{message}</p>
+        <div className="modal-buttons">
+          {phone && (
+            <button onClick={() => window.location.href = `tel:${phone}`}>Call {name}</button>
+          )}
+          {email && (
+            <button onClick={() => window.location.href = `mailto:${email}`}>Email {email}</button>
+          )}
         </div>
-       
-       
+      </div>
     </div>
-</div>
-  )
+    
+    
+      
+      )}
+    </div>
+  );
 }
-export default ContactComponent
+
+export default ContactComponent;
--- src/pages/Dashboard/Overview/Components/Leads/Leads.js
+++ src/pages/Dashboard/Overview/Components/Leads/Leads.js
@@ -6,42 +6,58 @@ import { saveAs } from 'file-saver';
 import axios from 'axios';
 
 const Leads = ({ userId }) => {
-  const [contactData, setContactData] = useState([]);
-  const [loading, setLoading] = useState(true); // Loading state
-  const contactsPerPage = 7; // Number of contacts per page
+  const [contactData, setContactData] = useState(null); // State for contact data
+  const [searchQuery, setSearchQuery] = useState(''); // State for search query
+  const [filterOption, setFilterOption] = useState('mostRecent'); // State for filter option
   const [currentPage, setCurrentPage] = useState(1); // Current page number
+  const [loading, setLoading] = useState(true); // Loading state
 
+  const contactsPerPage = 7; // Number of contacts per page
   const isMobile = window.innerWidth <= 1000;
 
   // Fetch contacts data
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchContactData = async () => {
       try {
         const response = await axios.get(`https://${process.env.REACT_APP_HOST}/api/${userId}/leads`);
         setContactData(response.data);
         setLoading(false);
       } catch (error) {
-        console.error('Error fetching contact data:', error);
+        console.log('Error fetching contact data:', error);
+        setLoading(false);
       }
     };
 
-    fetchData();
-
-    // Cleanup function
-    return () => {
-      // Cancel any pending requests or cleanup if necessary
-    };
+    fetchContactData();
   }, [userId]);
 
-  // Get contacts for the current page
+  // Filter contacts based on search query and filter option
+  const filteredContacts = contactData
+  ? contactData.filter(contact => {
+      return (
+        contact.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        contact.email.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        contact.company.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    }).sort((a, b) => {
+      if (filterOption === 'mostRecent') {
+        return new Date(b.contact_date) - new Date(a.contact_date); // Sort from most recent to oldest
+      } else {
+        return new Date(a.contact_date) - new Date(b.contact_date); // Sort from oldest to most recent
+      }
+    })
+  : [];
+
+  // Pagination
   const indexOfLastContact = currentPage * contactsPerPage;
   const indexOfFirstContact = indexOfLastContact - contactsPerPage;
-  const currentContacts = contactData.slice(indexOfFirstContact, indexOfLastContact);
-  const contactCounter = contactData.length;
+  const currentContacts = filteredContacts.slice(indexOfFirstContact, indexOfLastContact);
+  const contactCounter = currentContacts.length;
 
   // Change page
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const paginate = pageNumber => setCurrentPage(pageNumber);
 
+  // Download CSV function
   const downloadCSV = () => {
     if (contactData && contactData.length > 0) {
       const header = ['Name', 'Email', 'Company', 'Contact Date', 'Message'];
@@ -52,6 +68,8 @@ const Leads = ({ userId }) => {
           contact.name,
           contact.email,
           contact.company,
+          contact.sector,
+          contact.phone,
           contact.contact_date,
           contact.message
         ];
@@ -63,67 +81,98 @@ const Leads = ({ userId }) => {
       saveAs(blob, 'leads.csv');
     }
   };
-
-  return (
-    <div className='lead-container'>
-      <div className="lead-wrapper">
-        <div className="contact-description">
-          <h1>Name, Company</h1>
-          <h1>Email</h1>
-          <h1>Contact Date</h1>
-          <h1>Message</h1>
-        </div>
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          currentContacts.length > 0 ? (
-            currentContacts.map(contact => (
-              <ContactComponent
-                key={contact.contact_id}
-                name={contact.name}
-                email={contact.email}
-                company={contact.company}
-                contact_date={contact.contact_date}
-                message={contact.message}
-              />
-            ))
-          ) : (
-            <p>There is no data to show at the moment</p>
-          )
-        )}
-        <button className="download-csv-button" onClick={downloadCSV}>Export Leads</button>
-      </div>
-      {/* Pagination */}
-      {contactData && contactData.length > contactsPerPage && (
-        <div className="pagination-container">
-          <ul className="pagination">
-            {Array.from({ length: Math.ceil(contactData.length / contactsPerPage) }, (_, i) => (
-              <li
-                key={i}
-                className={currentPage === i + 1 ? 'active' : ''}
-                onClick={() => paginate(i + 1)}
-              >
-                {i + 1}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-       
-      <div className="overview-body-sidebar">
-        <section className="payment-section">
-          {!isMobile && (
-            <div>
-              <h2>GENERATED LEADS: </h2>
-              <div className='contact-counter'>{contactCounter}</div>
-            </div>
-          )}
-          <div className="payment-section-header">
-          </div>
-        </section>
-      </div>
+  const pagination = (
+    <div className="pagination-container">
+      <ul className="pagination">
+        {Array.from({ length: Math.max(1, Math.ceil(filteredContacts.length / contactsPerPage)) }, (_, i) => (
+          <li
+            key={i}
+            className={currentPage === i + 1 ? 'active' : ''}
+            onClick={() => paginate(i + 1)}
+          >
+            {i + 1}
+          </li>
+        ))}
+      </ul>
     </div>
   );
+
+  return (
+      <div className='lead-container'>
+        <div className="lead-header">
+          <h1 className='lead-title'>Card generated Leads</h1>
+        </div>
+        <div className="lead-wrapper">
+          {/* Filter container */}
+          <div className="filter-container">
+            <input
+              type="text"
+              placeholder="Search..."
+              value={searchQuery}
+              onChange={e => setSearchQuery(e.target.value)}
+              style={{ width: '100%' }} // Set the input width
+            />
+            <select
+              value={filterOption}
+              onChange={e => setFilterOption(e.target.value)}
+              style={{ height: '40px' }} // Set the dropdown height
+            >
+              <option value="mostRecent">Most Recent</option>
+              <option value="oldest">Oldest</option>
+            </select>
+          </div>
+          {/* Contact description */}
+          <div className="contact-description">
+            <h1>Name, Company</h1>
+            <h1>Email</h1>
+            <h1 style={{marginLeft:"40px"}}>Phone</h1>
+            <h1>Sector</h1>
+            <h1>Contact Date</h1>
+            <h1>Message</h1>
+          </div>
+          {/* Display contacts */}
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            currentContacts.length > 0 ? (
+              currentContacts.map(contact => (
+                <ContactComponent
+                  key={contact.contact_id} // Ensure the key is unique
+                  user_id={contact.user_id}
+                  contact_id={contact.contact_id}
+                  name={contact.name}
+                  company={contact.company}
+                  email={contact.email}
+                  sector={contact.sector}
+                  phone={contact.phone}
+                  message={contact.message}
+                  contact_date={contact.contact_date} // Ensure this prop is passed
+                />
+              ))
+            ) : (
+              <p>No results found.</p>
+            )
+          )}
+          {/* Export button */}
+          <button className="download-csv-button" onClick={downloadCSV}>Export Leads</button>
+        </div>
+        {/* Pagination */}
+        {pagination}
+        {/* Sidebar */}
+        <div className="overview-body-sidebar">
+          <section className="payment-section">
+            {!isMobile && (
+              <div>
+                <h2>GENERATED LEADS: </h2>
+                <div className='contact-counter'>{contactData?.length ?? 0}</div>
+              </div>
+            )}
+            <div className="payment-section-header"></div>
+          </section>
+        </div>
+      </div>
+  );
+  
 };
 
 export default Leads;
