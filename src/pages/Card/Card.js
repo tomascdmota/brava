27299ae031c898cba118cbc22b78d214ef5c4e71
tomascdmota <tomasmota@outlook.com>
2024-09-20 19:06:02 +0100

    Changed card route

--- src/pages/Card/Card.js
+++ src/pages/Card/Card.js
@@ -1,69 +1,51 @@
 import React, { useEffect, useState, lazy, Suspense } from 'react';
-import { useParams, useNavigate } from 'react-router';
+import { useParams } from 'react-router';
 import axios from 'axios';
 
 import './Card.scss';
 const CardComponent = lazy(() => import('./CardComponent/CardComponent'));
 
 function Cards() {
-  const { id: userId } = useParams();
-  const [userData, setUserData] = useState(null); // Initialize as null
-  const [cards, setCards] = useState([]);
-  const [linkId, setLinkId] = useState('')
+  const { id: userId, cardId } = useParams();
+  const [cardData, setCardData] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    console.log(userId)
     axios
-      .get(`https://${process.env.REACT_APP_HOST}/api/${userId}/cards`, { withCredentials: true })
+      .get(`http://srv597605.hstgr.cloud:4001/api/card/${cardId}`)
       .then((response) => {
-        setUserData(response.data);
-        setCards(response.data.cards);
-        setLinkId(response.data.linkId);
-        
-  
+        const card = response.data; // Directly use the card object
+        setCardData(card);
+
         // Preload LCP image
-        const lcpImageUrls = response.data.cards.map((card) => card.profile_image_url);
-  
-        lcpImageUrls.forEach((imageUrl) => {
-          const preloadLink = document.createElement('link');
-          preloadLink.href = imageUrl;
-          preloadLink.rel = 'preload';
-          preloadLink.as = 'image';
-          document.head.appendChild(preloadLink);
-        });
+        const preloadLink = document.createElement('link');
+        preloadLink.href = card.profile_image_url; // Preload the profile image
+        preloadLink.rel = 'preload';
+        preloadLink.as = 'image';
+        document.head.appendChild(preloadLink);
       })
       .catch((error) => {
-        console.error('Error fetching cards:', error);
+        console.error('Error fetching card:', error);
       })
       .finally(() => {
-        setLoading(false); // Set loading to false regardless of success or failure
+        setLoading(false);
       });
+  }, [cardId]);
 
-
-  }, [userId]);
-  console.log(linkId)
-  // Render loading indicator while data is being fetched
   if (loading) {
     return (
       <div className="cards-container">
-        <h1>Loading...</h1>
         <div className="spinner-container"><div className="spinner"></div></div>
       </div>
     );
   }
-  // Once data is fetched, render the page with the fetched data
+
+  // Once data is fetched, render the card with the fetched data
   return (
     <div className="cards-container">
-      <div className="nav-tiles">
-        <div className="tiles">
-          {cards.map((card) => (
-            <Suspense key={card.card_id} fallback={<div>Loading card...</div>}>
-              <CardComponent linkId={linkId} {...card} />
-            </Suspense>
-          ))}
-        </div>
-      </div>
+      <Suspense fallback={<div>Loading card...</div>}>
+        <CardComponent {...cardData} /> {/* Spread the card data directly */}
+      </Suspense>
     </div>
   );
 }
